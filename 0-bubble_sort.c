a4e635f6fcc508b338f209586dbd30bed9cea9ea
Emeremnu Edison Maduabuchi <edisonemeremnu@gmail.com>
2024-01-22 20:13:03 +0100

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,13 +8,14 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
+    size_t i, j, flag;
 
     if (array == NULL || size < 2)
         return;
 
     for (i = 0; i < size -1; i++)
     {
+        flag = 0;
         for (j = 0; j < size - 1 - i; j++)
         {
             if (array[j] > array[j + 1])
@@ -22,8 +23,12 @@ void bubble_sort(int *array, size_t size)
                 int temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
+                flag = 1;
             }
         }
+        if (flag == 0)
+            break;
+
         print_array(array, size);
     }
 
